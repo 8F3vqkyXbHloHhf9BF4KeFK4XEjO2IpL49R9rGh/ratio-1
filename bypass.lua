d386efbd2e6fc737a8617f6542432633b744054a
wednesdaytruth <105751066+wednesdaytruth@users.noreply.github.com>
2022-08-20 11:52:10 -0400

    Update bypass.lua

--- bypass.lua
+++ bypass.lua
@@ -1,8 +1,8 @@
 repeat task.wait() until game:IsLoaded() -- Could be more optimized but it's not gonna last very long so idc
 _G.oldpcall = getrenv().pcall
-local old = getrenv().pcall
+old = getrenv().pcall
 getrenv().pcall = newcclosure(function(...)
-    local args = {...}
+    args = {...}
     if not checkcaller() then
         for _, c in pairs(getconstants(args[1])) do
             if c == "FindService" or c == "GetService" then
