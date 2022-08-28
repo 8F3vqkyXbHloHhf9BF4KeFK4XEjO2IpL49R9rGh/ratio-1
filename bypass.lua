a30cb709815cdef81469172f57749e92714a20b4
wednesdaytruth <105751066+wednesdaytruth@users.noreply.github.com>
2022-08-27 20:19:12 -0400

    Update bypass.lua

--- bypass.lua
+++ bypass.lua
@@ -1,5 +1,5 @@
 repeat task.wait() until game:IsLoaded() -- Could be more optimized but it's not gonna last very long so idc
-if game.PlaceId == 379614936 or game.PlaceId == 379614936 or game.PlaceId == 5006801542 or game.PlaceId == 860428890 then
+if game.PlaceId == 379614936 or game.PlaceId == 379614936 or game.PlaceId == 5006801542 or game.PlaceId == 860428890 or game.PlaceId == 866472074 then
 _G.oldpcall = getrenv().pcall
 old = getrenv().pcall
 getrenv().pcall = newcclosure(function(...)
